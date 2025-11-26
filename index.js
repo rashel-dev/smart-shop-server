575e28aeeac7d22d31a2c7687b28543f72b2ad0a
rashel9255 <ibrahim2000167@gmail.com>
2025-11-27 00:52:30 +0600

    feat: Implement initial Express server with MongoDB connection, all product fetching, and basic project setup.

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0ocgkty.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Smart Shop BD!");
+});
+
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db(`${process.env.DB_NAME}`);
+        const productsCollection = database.collection("products");
+
+        app.get("/products", async (req, res) => {
+            try {
+                const cursor = productsCollection.find({});
+                const products = await cursor.toArray();
+                res.send(products);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+                res.status(500).json({ error: "Failed to fetch products" });
+            }
+        });
+
+        
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Smart Shop BD is running on port ${port}`);
+});
